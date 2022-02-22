83b5665fff1c06baa9189e245830f41cc69871d3
dzagardo <dzagardo@andrew.cmu.edu>
2022-02-22 15:22:47 -0500

    deleted dave_extension folder. not needed.

--- hw1/dave_extension/popup.js
+++
@@ -1,8 +0,0 @@
-// Display a popup when this page is loaded.
-chrome.browserAction.onLoaded.addListener(
-    function(tab) {
-        chrome.browserAction.setPopup({
-            popup: "popup-window.html"
-        });
-    }
-)
