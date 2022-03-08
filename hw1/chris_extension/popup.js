f82af5937696de0c054a68ff7a3ccb17400b0940
Chris Choy <contact@chrischoy.net>
2022-03-08 11:48:08 -0600

    revert hw1 directory to submission version

---
+++ hw1/chris_extension/popup.js
@@ -0,0 +1,8 @@
+// Display a popup when this page is loaded.
+chrome.browserAction.onLoaded.addListener(
+    function(tab) {
+        chrome.browserAction.setPopup({
+            popup: "popup-window.html"
+        });
+    }
+)
