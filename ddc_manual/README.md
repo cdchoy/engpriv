1e9a41369c9ed4ef9c3d17999b4c4da008b823de
Chris Choy <contact@chrischoy.net>
2022-03-16 22:56:28 -0400

    detect url of current tab. change button.css to style.css

--- ddc_manual/README.md
+++ ddc_manual/README.md
@@ -7,7 +7,7 @@ Instead, it uses a maintained file that maps web domains to their:
 - RTK online request form URL
 - Contact Email for exercising CCPA rights
 
-Chris is re-building the extension mostly from scratch here to both (1) eliminate unused scraper code, 
+Chris is re-building the extension mostly from scratch here to both (1) eliminate unused scraper code,
 and (2) learn how to build an extension himself.
 
 ## File Structure
@@ -28,3 +28,5 @@ ddc_manual/
 ## Resources
 
 - [Chrome Extension Developers: Getting Started](https://developer.chrome.com/docs/extensions/mv3/getstarted/)
+- [ManifestV3 Field Summary](https://developer.chrome.com/docs/extensions/mv3/manifest/)
+- [Chrome API Reference](https://developer.chrome.com/docs/extensions/reference/)
--- ddc_manual/src/background.js
+++ ddc_manual/src/background.js
@@ -1,8 +1,34 @@
 // background.js
 
-let color = '#3aa757';
-
+// * * Event Listeners * * //
 chrome.runtime.onInstalled.addListener(() => {
+  let color = '#3aa757';
   chrome.storage.sync.set({ color });
   console.log('Default background color set to %cgreen', `color: ${color}`);
 });
+
+chrome.tabs.onActivated.addListener(() => {
+  console.log('Tab Activated!');
+  updateCurrentUrl()
+})
+
+chrome.tabs.onUpdated.addListener(() => {
+  console.log('Tab Updated!');
+  updateCurrentUrl()
+})
+
+// * * Functions * * //
+async function updateCurrentUrl() {
+  let url = await chrome.tabs.query({active: true, currentWindow: true})
+    .then((tabs) => { // get current tab
+      if (!tabs.length) return;
+      return tabs[0]; // current tab should be first and only in list
+    })
+    .then(tab => { // extract url from tab
+      if (!tab) return;
+      return tab.url
+    })
+  console.log('Current page url:', url)
+  chrome.storage.sync.set({url})
+}
+
