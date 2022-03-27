30f00ccc173703b8e1565ab327fdb446da141db7
dzagardo <dzagardo@andrew.cmu.edu>
2022-03-27 16:40:13 -0400

    Should correctly push the React branch now. Trying to fix React Hooks. Need to figure some things out and this might break the current build.

---
+++ engpriv/hw1/data_daddy_ccpa/src/background.js
@@ -0,0 +1,23 @@
+// background.js
+function changeColor(newColor) {
+  var elem = document.getElementById('para');
+  elem.style.color = newColor;
+  element.console.log("fuck")
+}
+
+function log(newLog) {
+  alert("fuck")
+  var element = document.getElementById('log');
+  element.console.log(newLog)
+}
+
+// chrome.runtime.onInstalled.addListener(function() {
+//     chrome.button.create({
+//       target: {button: button.id},
+//       files: ['content.js']
+//     });
+//   });
+
+//   chrome.generateEmail.onClicked.addListener(function() {
+//     console.log("reeee")
+//   });
