a927d167b0b54616fe0cf926f2655dabe43da124
dzagardo <dzagardo@andrew.cmu.edu>
2022-03-27 17:01:11 -0400

    Wow I suck at git.

---
+++ Dropbox/My Mac (Davids-MacBook-Pro.local)/Desktop/SCHOOL/SPRING2022/EPS/engpriv/hw1/data_daddy_ccpa/webpack.config.js
@@ -0,0 +1,24 @@
+// webpack.config.js
+const path = require('path');
+module.exports = {
+    context: __dirname,
+
+    entry: {
+        index: './src/index.js',
+        about: './src/about.js'
+    },
+
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+            }
+        ]
+    },
+    plugins: []
+};
