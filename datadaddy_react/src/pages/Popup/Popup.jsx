990fede80eb8116ee4bc26851f343fba465ff959
dzagardo <dzagardo@andrew.cmu.edu>
2022-04-06 16:14:07 -0400

    Adding buttons for hardcode

--- datadaddy_react/src/pages/Popup/Popup.jsx
+++ datadaddy_react/src/pages/Popup/Popup.jsx
@@ -186,15 +186,9 @@ const Popup = () => {
 
       </p>
 
-      <a
-        className="App-link"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
         <div>
           <div>
-            <TextField id="userName" label="Your Name" variant="outlined" sx={{ width: 250, height: 50, mt: 1 }} />
-            <FormControl sx={{ minWidth: 250, height: 50, mt: 2 }}>
+            {/* <FormControl sx={{ minWidth: 250, height: 50, mt: 2 }}>
               <InputLabel id="dataBrokers">Select Broker</InputLabel>
               <Select
                 labelId="dataBrokers"
@@ -208,15 +202,18 @@ const Popup = () => {
                 <MenuItem value={databroker_map["db3"]}>db3</MenuItem>
                 <MenuItem value={databroker_map["db4"]}>db4</MenuItem>
               </Select>
-            </FormControl>
+            </FormControl> */}
           </div>
-          <MuiButton variant="outlined" onClick={generateEmail} endIcon={<SendIcon />} sx={{ width: 250, height: 56, mt: 2 }}>OPEN EMAIL IN NEW TAB</MuiButton>
         </div>
+        <TextField id="userName" label="Your Name" variant="outlined" sx={{ width: 250, height: 56, mt: 1 }} />
+        <MuiButton id="privacypolicy" variant="outlined" sx={{ width: 250, height: 56, mt: 2 }}>Loading...</MuiButton>
+        <MuiButton id="onlineform" variant="outlined" sx={{ width: 250, height: 56, mt: 2 }}>Loading...</MuiButton>
+        <MuiButton id="emailrequest" variant="outlined" sx={{ width: 250, height: 56, mt: 2 }}>Loading...</MuiButton>
+        <MuiButton variant="outlined" onClick={generateEmail} endIcon={<SendIcon />} sx={{ width: 250, height: 56, mt: 2 }}>OPEN EMAIL IN NEW TAB</MuiButton>
 
         <footer className='App-footer'>
           <MuiButton variant="contained" onClick={scrape} sx={{ width: 250, height: 50, alignContent: 'flex-start', mt: 1 }}>Scrape</MuiButton>
         </footer>
-      </a>
     </div>
   );
 };
