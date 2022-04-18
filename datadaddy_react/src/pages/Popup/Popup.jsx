41c7a97881a1696ae11e7d0589c2ff2689427bc4
Chris Choy <chrischoythehuman@gmail.com>
2022-04-18 19:51:54 -0400

    popup can read domain from chrome.storage! squash react warning about <text> tag

--- datadaddy_react/src/pages/Popup/Popup.jsx
+++ datadaddy_react/src/pages/Popup/Popup.jsx
@@ -9,60 +9,73 @@ import generateEmail from './GenerateEmail';
 import ScrapeButton from './Scraper';
 import { domainData } from '../../assets/data/domainMap';
 
-const Popup = () => {
-  let ppHref;
-  let onlineFormHref;
-  let emailHref;
+export default class Popup extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      domain: "loading...",
+    };
+  }
 
-  ppHref = "https://www.redditinc.com/policies/privacy-policy";
-  emailHref = generateEmail(["privacy@towerdata.com"]);
+  componentDidMount() {
+    this.setDebugDefaults();
+    this.timerID = setInterval(
+      () => this.tick(),
+      500
+    )
+  }
+
+  setDebugDefaults() {
+    this.ppHref = "https://www.redditinc.com/policies/privacy-policy";
+    this.emailHref = generateEmail(["privacy@towerdata.com"])
+  }
+
+  tick() {
+    chrome.storage.sync.get('domain', (results) => {
+      if (results.domain) {
+        this.setState({
+          domain: results.domain,
+        })
+      }
+    })
+  }
+
+  getDomainInfo() {
+    for (let item in domainData) {
+      console.log(item);
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <script defer src="./dist/bundle.js" />
+          <DataDaddyLogo/>
+        </header>
   
-  return (
-    <div className="App">
-      <header className="App-header">
-        <script defer src="./dist/bundle.js" />
-        <DataDaddyLogo/>
-      </header>
-
-      <p>You are on {window.location.hostname}</p>
-      <TextField id="userName" label="Your Name" variant="outlined" sx={{ width: 250, height: 56, mt: 1 }} />
-      <PopupButton text="Privacy Policy" href={ppHref}/>
-      <PopupButton text="Online Form" href={onlineFormHref}/>
-      <PopupButton text="Email Request" icon={<SendIcon/>} href={emailHref}/> 
-
-      <footer className='App-footer'>
-        <ScrapeButton/>
-      </footer>
-    </div>
-  );
+        <p>You are on {this.state.domain}</p>
+        <TextField id="userName" label="Your Name" variant="outlined" sx={{ width: 250, height: 56, mt: 1 }} />
+        <PopupButton text="Privacy Policy" href={this.ppHref}/>
+        <PopupButton text="Online Form" href={this.onlineFormHref}/>
+        <PopupButton text="Email Request" icon={<SendIcon/>} href={this.emailHref}/> 
+  
+        <footer className='App-footer'>
+          <ScrapeButton/>
+        </footer>
+      </div>
+    );
+  }
 };
 
-function DataDaddyLogo() {
+const DataDaddyLogo = () => {
   return (
     <p className="Logo-text">
-      <text className="App-text-one Gray">datadaddy.</text>
-      <text className="App-text-two Blue">CC</text>
-      <text className="App-text-two Red">P</text>
-      <text className="App-text-two Blue">A</text>
+      <span className="App-text-one Gray">datadaddy.</span>
+      <span className="App-text-two Blue">CC</span>
+      <span className="App-text-two Red">P</span>
+      <span className="App-text-two Blue">A</span>
       <SettingsIcon variant="outlined" className="Gray"/>
     </p>
   )
 }
-
-function getDomainInfo() {
-  for (let item in domainData) {
-    console.log(item);
-  }
-}
-
-const getDomain = () => {
-	chrome.storage.sync.get('domain', (results) => {
-		if (results.domain) {
-			this.setState({
-				domain: results.domain,
-			})
-		}
-	})
-}
-
-export default Popup;
